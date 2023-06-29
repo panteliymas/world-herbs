73ee370b66a002c8adcd4ccb2f0ae374c1265f15
panteliymas <panteliymas@panteliymas.xyz>
2023-06-29 20:06:34 +0000

    module WorldHerbs created, so laravel can be updated w/o problems

---
+++ Modules/WorldHerbs/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('worldherbs::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('worldherbs.name') !!}
+    </p>
+@endsection
